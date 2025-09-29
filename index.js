f1569219afd4899f1a84a0ee79d5a7a1bbec9b39
Edwingc320 <130888359+Edwingc320@users.noreply.github.com>
2025-09-28 19:13:18 -0600

    Update index.js
    Corrección de mensaje de puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 var app = express();
 var server = app.listen(4000, function(){
-    console.log('Servidor corriendo en http://localhost:5000');
+    console.log('Servidor corriendo en http://localhost:4000');
 });
 
 app.use(express.static('public'));
